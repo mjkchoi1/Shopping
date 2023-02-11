1e5575ffbcb74e2a8afc37c7ee5942495a7f8e69
mjkchoi1 <mjc@njit.edu>
2023-02-11 17:59:26 -0500

    Testing problem 1
    Signed-off-by: mjkchoi1 <mjc@njit.edu>

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -8,6 +8,11 @@ function processArray($arr) {
     echo "<br>Odds output:<br>";
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
+    foreach($arr as $val){
+        if($val%2==1)
+        echo $val;
+    }
+
 
 }
 echo "Problem 1: Odd Output<br>";
